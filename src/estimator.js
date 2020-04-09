af7d6c2d0411af91ca14956640ec55bb42919aa8
Pelumi Adebayo <adepelumi1996@gmail.com>
2020-04-09 03:44:21 +0100

    Eslint Error fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,16 @@
 const factor = (days) => Math.trunc(days / 3);
 
 
-const covid19ImpactEstimator = (data) => {
-return{
-    data: data, 
-    impact: {
-        currentlyInfected: data.reportedCases * 10, 
-        infectionsByRequestedTime: this.currentlyInfected * (Math.pow(2,factor(28))) 
-    }, 
-    severeImpact: {
-        currentlyInfected: data.reportedCases * 50, 
-        infectionsByRequestedTime: this.currentlyInfected * (Math.pow(2,factor(28)))
-    } 
-};
-};
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: this.currentlyInfected * (2 ** factor(28))
+  },
+  severeImpact: {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: this.currentlyInfected * (2 ** factor(28))
+  }
+});
 
 export default covid19ImpactEstimator;
