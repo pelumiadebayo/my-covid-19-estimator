7bfe65135871484d4f85ae7afac0930d787d9e69
Pelumi Adebayo <adepelumi1996@gmail.com>
2020-04-09 03:16:14 +0100

    Error fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,20 +1,18 @@
-const factor = (days)=>{
-    return Math.trunc(days/3);
-};
+const factor = (days) => Math.trunc(days / 3);
 
-const covid19ImpactEstimator = (data) =>{
-const input = data;
-    return{
-        data: input,
-        impact: {
-            currentlyInfected: input.reportedCases * 10,
-            infectionsByRequestedTime: this.currentlyInfected * (Math.pow(2,factor(28))) 
-        }, 
-        severeImpact: {
-            currentlyInfected: input.reportedCases * 50,
-            infectionsByRequestedTime: this.currentlyInfected * (Math.pow(2,factor(28)))
-        } 
-    }
+
+const covid19ImpactEstimator = (data) => {
+return{
+    data: data, 
+    impact: {
+        currentlyInfected: data.reportedCases * 10, 
+        infectionsByRequestedTime: this.currentlyInfected * (Math.pow(2,factor(28))) 
+    }, 
+    severeImpact: {
+        currentlyInfected: data.reportedCases * 50, 
+        infectionsByRequestedTime: this.currentlyInfected * (Math.pow(2,factor(28)))
+    } 
+};
 };
 
 export default covid19ImpactEstimator;
