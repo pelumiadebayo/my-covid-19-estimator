e7f1e74529ab6f0ff4bfaaea5a41168c8992a230
Pelumi Adebayo <adepelumi1996@gmail.com>
2020-04-09 02:45:29 +0100

    Challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,20 @@
-const covid19ImpactEstimator = (data) => data;
+const factor = (days)=>{
+    return Math.trunc(days/3);
+};
+
+const covid19ImpactEstimator = (data) =>{
+const input = data;
+    return{
+        data: input,
+        impact: {
+            currentlyInfected: input.reportedCases * 10,
+            infectionsByRequestedTime: this.currentlyInfected * (Math.pow(2,factor(28))) 
+        }, 
+        severeImpact: {
+            currentlyInfected: input.reportedCases * 50,
+            infectionsByRequestedTime: this.currentlyInfected * (Math.pow(2,factor(28)))
+        } 
+    }
+};
 
 export default covid19ImpactEstimator;
